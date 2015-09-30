4d9d10f55144dbb7b690f5a490a7d5ef718ab613
Sonja Krause-Harder <skh@rcane.de>
2015-09-30 20:35:06 +0200

    Add auth blueprint skeleton

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
