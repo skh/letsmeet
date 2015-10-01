63aa48a45a8c6bbc5c654c76148cd1bfc5a6082a
Sonja Krause-Harder <skh@rcane.de>
2015-10-01 20:43:10 +0200

    Add login manager skeleton

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask.ext.login import LoginManager
 from config import config
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    login_manager.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
---
+++ app/auth/views.py
@@ -0,0 +1,19 @@
+from flask import redirect, request, url_for
+from flask.ext.login import login_user, logout_user, login_required, \
+    current_user
+from . import auth
+from .forms import LoginForm
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
