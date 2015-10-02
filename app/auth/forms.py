d9f7f09ad7f5958f7de4f22a8cf3e65783c3f033
Sonja Krause-Harder <skh@rcane.de>
2015-10-02 20:43:57 +0200

    Remove forms for now

--- app/auth/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-
-class LoginForm(Form):
-    email = StringField('Email')
-    password = PasswordField('Password')
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Login')
