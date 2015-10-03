4f6c5213f4e56f3f5c3a1e6ef25ac0bf55c3af17
Sonja Krause-Harder <skh@rcane.de>
2015-10-03 10:49:25 +0200

    Make topbar dropdowns open and close on click

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,8 @@
 /* script.js */
+// see https://github.com/zurb/foundation/issues/5551
+// workaround to make topbar dropdowns close on second click
+$( 'nav.top-bar:not(.expanded)>section>ul>li.has-dropdown a' ).on( 'click', function( e ) {
+        var menu = $( 'nav.top-bar>section>ul>li.has-dropdown ul' ).foundation( 'dropdown' );
+        menu.toggle();
+    }
+);
