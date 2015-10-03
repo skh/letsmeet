92356b5e780d74d969b8ca9931daa75c0587cb30
Sonja Krause-Harder <skh@rcane.de>
2015-10-03 13:58:39 +0200

    Add mock data

---
+++ app/static/js/mockdata.js
@@ -0,0 +1,23 @@
+var meetings = [
+	{
+		"title": "Meeting 1",
+		"attendees": [1, 2, 3],
+		"date": "Mon, 26 Oct 2015 13:00:00 GMT"
+	},
+];
+
+var users = [
+	{
+		"id": 1,
+		"email": "krauseha@gmail.com",
+		"name": "Sonja GMail"
+	}, {
+		"id": 2,
+		"email": "sonja.krause-harder@open-xchange.com",
+		"name": "Sonja OX"
+	}, {
+		"id": 3,
+		"email": "skh@rcane.de",
+		"name": "Sonja rcane"
+	}
+];
