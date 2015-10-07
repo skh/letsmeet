857b0620dfec119ced3b142de477da39486b05b3
Sonja Krause-Harder <skh@rcane.de>
2015-10-07 19:56:50 +0200

    Add mail handling (untested)

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,15 @@
+from flask import make_response
 from . import api
 
-@api.route('/parsemail')
+@api.route('/parsemail', methods=['POST'])
 def parsemail():
-    return '{"content": "yeah!"}'
+    # see https://documentation.mailgun.com/quickstart-receiving.html
+    if request.method == 'POST':
+         sender = request.form['sender']
+         subject = request.form['subject']
+         message-headers = request.form['message-headers']
+         body_plain = request.form['body-plain']
+
+     # Returned text is ignored but HTTP status code matters:
+     # Mailgun wants to see 2xx, otherwise it will make another attempt in 5 minutes
+     return make_response('OK', 200)
