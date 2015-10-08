bda1220f93b430eea2548bf29d83bdf804cca160
Sonja Krause-Harder <skh@rcane.de>
2015-10-08 19:44:42 +0200

    Only start app from shell if in __main__

--- letsmeet.py
+++ letsmeet.py
@@ -2,13 +2,12 @@ from app import create_app, db
 
 from flask.ext.script import Manager, Shell
 
-app = create_app('default')
-manager = Manager(app)
-
-def make_shell_context():
-    return dict(app=app,db=db)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
 if __name__ == '__main__':
+
+    app = create_app('default')
+    manager = Manager(app)
+
+    def make_shell_context():
+        return dict(app=app,db=db)
+    manager.add_command("shell", Shell(make_context=make_shell_context))
     manager.run()
