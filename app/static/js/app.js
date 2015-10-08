637c7233bca70a28ea71a1c884f79e3adc6af564
Sonja Krause-Harder <skh@rcane.de>
2015-10-09 00:03:13 +0200

    Add frontend scripting

---
+++ app/static/js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+
+	var Meeting = function () {};
+
+	var ViewModel = function () {
+		this.meetings = ko.observableArray();
+		this.data = ko.observable();
+		this.loadMeetings();
+	};
+	ViewModel.prototype.loadMeetings = function () {
+		$.ajax({
+			url: '/meetinglist',
+			method: 'GET',
+			done: function (data) {
+				this.json(data);
+			}
+		});
+	};
+
+	var m = new ViewModel();
+
+	ko.applyBindings(m);
+
+});
