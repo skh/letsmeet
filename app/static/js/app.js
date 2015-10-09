55b98509b581be6c48981be9517e41e4cdcf4d3a
Sonja Krause-Harder <skh@rcane.de>
2015-10-09 03:01:22 +0200

    add custom bindinghandler

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,8 +1,32 @@
 $(function () {
+	
+	// wrapper to hasFocus that also selects text and applies focus async
+	ko.bindingHandlers.selectAndFocus = {
+		init: function (element, valueAccessor, allBindingsAccessor, bindingContext) {
+			ko.bindingHandlers.hasFocus.init(element, valueAccessor, allBindingsAccessor, bindingContext);
+			ko.utils.registerEventHandler(element, 'focus', function () {
+				element.focus();
+			});
+		},
+		update: function (element, valueAccessor) {
+			ko.utils.unwrapObservable(valueAccessor()); // for dependency
+			// ensure that element is visible before trying to focus
+			setTimeout(function () {
+				ko.bindingHandlers.hasFocus.update(element, valueAccessor);
+			}, 0);
+		}
+	};
+
 	var Meeting = function (meeting) {
+		this.id = meeting.id;
 		this.title = ko.observable(meeting.title);
 		this.text = ko.observable(meeting.text);
 		this.editing = ko.observable(false);
+		this.save = function () {
+			$.post('/meeting/' + this.id, {
+				text: this.text()
+			});
+		};
 	}
 	var ViewModel = function () {
 		var self = this;
@@ -36,6 +60,7 @@ $(function () {
 		}
 
 		this.saveEditMeetingText = function(meeting) {
+			meeting.save();
 			meeting.editing(false);
 		}
 
@@ -49,5 +74,7 @@ $(function () {
 	var m = new ViewModel();
 
 	ko.applyBindings(m);
+
+
 
 });
