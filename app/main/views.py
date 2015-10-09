c867d4eb082e3bd22d63f82a0d50698aa4fe5816
Sonja Krause-Harder <skh@rcane.de>
2015-10-09 03:13:03 +0200

    Import request before use

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify, make_response
+from flask import render_template, jsonify, make_response, request
 from flask.ext.login import current_user, login_required
 from . import main
 from .. import db, login_manager
